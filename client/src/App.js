46e0ec190e4322228fdda95929b33b3caf9c9921
Desire <desireirankundawilliams@gmail.com>
2022-10-13 01:11:31 +0200

    able to created componrnts

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import Header from "./Component/Header";
+import Clients from "./Component/Clients";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-    hello
+      <ApolloProvider client={client}>
+        <Header />
+        <Clients />
+      </ApolloProvider>
     </>
   );
 }
