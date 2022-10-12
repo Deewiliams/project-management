4c8b46507a69dfb3014770f39f5d5a2afcc9da0b
Desire <desireirankundawilliams@gmail.com>
2022-10-12 23:06:08 +0200

    able to query project info

--- schema/schema.js
+++ schema/schema.js
@@ -7,21 +7,46 @@ const {
   GraphQLList,
 } = require("graphql");
 
-//client Type
+//project Type
 
-const ClientType = new GraphQLObjectType({
-  name: "Client",
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    email: { type: GraphQLString },
-    phone: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
   }),
 });
+
+//client Type
+
+const ClientType = new GraphQLObjectType({
+    name: "Client",
+    fields: () => ({
+      id: { type: GraphQLID },
+      name: { type: GraphQLString },
+      email: { type: GraphQLString },
+      phone: { type: GraphQLString },
+    }),
+  });
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+        type: new GraphQLList(ProjectType),
+        resolve(parent, args) {
+          return projects;
+        },
+      },
+      project: {
+        type: ProjectType, 
+        args: { id: { type: GraphQLID } },
+        resolve(parent, args) {
+          return projects.find((project) => project.id === args.id);
+        },
+      },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
