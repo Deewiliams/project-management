f8aad3c6f81514d2525912976480dc83ad2e09f5
Desire <desireirankundawilliams@gmail.com>
2022-10-12 23:48:42 +0200

    connect mongoDb database

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
+const colors = require('colors')
+const connectDB = require('./server/config/db')
 const schema = require('./schema/schema')
 const port = process.env.PORT || 5000
 
 const app = express();
+
+//connecting to db
+connectDB();
 
 
 app.use('/graphql', graphqlHTTP({
