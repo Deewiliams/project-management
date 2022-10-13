a41e64450c9c0bb1d28c0920b094911b7d47508f
Desire <desireirankundawilliams@gmail.com>
2022-10-13 12:37:25 +0200

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-
+import Clients from './Component/Clients'
+import Header from './Component/Header';
 function App() {
   return (
     <div className="App">
      <h1>Hello world</h1>
+     <Header />
+     <Clients />
     </div>
   );
 }
