64bce9357d936fe0940e45bdadc4a5adbeb4ee35
Desire <desireirankundawilliams@gmail.com>
2022-10-13 14:56:40 +0200

    created an add client page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import Clients from './Component/client/Index'
 import Header from './Component/Header/Index';
+import AddClient from './Component/addClientModel/Index';
 function App() {
   return (
     <div className="App">
      <h1>Hello world</h1>
      <Header />
+     <AddClient />
+     <br />
      <Clients />
     </div>
   );
