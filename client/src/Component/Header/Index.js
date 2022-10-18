621ded9c7c6d3f931445e84390c22bf1354d0e06
Desire <desireirankundawilliams@gmail.com>
2022-10-18 10:25:40 +0200

    added a link to an icon

--- client/src/Component/Header/Index.js
+++ client/src/Component/Header/Index.js
@@ -3,10 +3,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
 import MenuIcon from "@material-ui/icons/Menu";
 import Add from "../add/Index";
 import AddProject from "../AddProject/Index";
-import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import AccountTreeIcon from "@material-ui/icons/AccountTree";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,18 +28,24 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar>
         <Toolbar>
-          <AccountTreeIcon
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </AccountTreeIcon>
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            <AccountTreeIcon
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </AccountTreeIcon>
+          </Link>
           <Typography variant="h6" className={classes.title}>
-            Project management app
+            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+              Project management app
+            </Link>
           </Typography>
-          <div style={{display: 'flex', justifyContent: 'center', padding: 20 }}>
+          <div
+            style={{ display: "flex", justifyContent: "center", padding: 20 }}
+          >
             <Add />
             <AddProject />
           </div>
