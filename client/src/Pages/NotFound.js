7ae8ecf385736974442c2e602bcf2c916e293d33
Desire <desireirankundawilliams@gmail.com>
2022-10-18 14:11:51 +0200

    worked on notfound page

--- client/src/Pages/NotFound.js
+++ client/src/Pages/NotFound.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div style={{ marginTop: 100, textAlign: "center" }}>
+      <div>
+        <h1>404</h1>
+        <h1>Page not found</h1>
+      </div>
+      <Link to="/">
+        <Button variant="contained" color="primary">
+          <ArrowBackIcon />
+          Go Back
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
